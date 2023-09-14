0bd8d3cf0bef3178d5b4bf1742f30fa1635d81be
vietnguyen2001 <101422985+vietnguyen2001@users.noreply.github.com>
2023-09-14 16:26:46 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ $(".readmore-btn").on('click', function() {
     var replaceText = $(this).parent().hasClass("showContent") ? "Xem toàn bộ" : "1";
     $(this).text(replaceText);
 });
+
+javascript
+const list = document.getElementById("list");
+const items = list.getElementsByTagName("li");
+
+function removeLastTwoIteams() {
+    for (let i = 0; i < 2; i++) {
+        list.removeChild(items[items.length - 1]);
+    }
+    rearrangeItems();
+}
+
+function rearrangeItems() {
+    for (let i = 0; i < items.length; i++) {
+        items[i].style.display = 'inline-block';
+    }
+}
